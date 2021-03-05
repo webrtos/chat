5d28b86d414316443a70e2d5e37d575c70c48a51
webrtos <admin@webrtos.com>
2021-03-05 15:00:36 +0200

    repo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // https://github.com/ipfs/js-ipfs/tree/master/docs/core-api
 // https://github.com/ipfs/js-ipfs/tree/master/examples
-const Ipfs = require('./libs/ipfs-0.54.3-rc.5-index.min')
+// https://www.jsdelivr.com/package/npm/ipfs
+// https://cdnjs.com/libraries/ipfs
+//const Ipfs = require('./libs/ipfs-0.54.3-rc.5-index.min')
 
 class chat {
 
@@ -9,12 +11,20 @@ constructor(opt) {
     if (npm_package_name)    this.name    = npm_package_name
     if (npm_package_version) this.version = npm_package_version
     if (npm_package_date)    this.date    = npm_package_date
-    //
-    this.ipfs = Ipfs
 }
 
-test() {
-    return this.opt
+async create(room) {
+    this.room = typeof room === 'string' ? room : 'room-' + Math.floor(Math.random() * 1e15).toString(36)
+    this.repo = Math.floor(Math.random() * 1e15).toString(36) + Math.floor(Math.random() * 1e15).toString(36)
+    this.node = await this.opt.ipfs.create({
+        repo: this.repo,
+        //init: {algorithm: 'ed25519'}
+    })
+    //const status = this.node.isOnline() ? 'online' : 'offline'
+    //const { cid } = await node.add('Hello world!')
+    //const data = await node.cat('QmQzCQn4puG4qu8PVysxZmscmQ5vT1ZXpqo7f58Uh9QfyY') // 'Hello world!'
+    //console.log(data) // data[0].toString()
+    return {room: this.room, repo: this.repo}
 }
 
 }
