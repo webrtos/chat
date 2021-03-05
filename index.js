5e9c8a16b2b23dde6852da715b5e8a9c47e99ef1
webrtos <admin@webrtos.com>
2021-03-05 16:14:17 +0200

    pubsub

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
 // https://cdnjs.com/libraries/ipfs
 //const Ipfs = require('./libs/ipfs-0.54.3-rc.5-index.min')
 
+const lib = {
+
+rand58: len => String.fromCharCode.apply(null, window.crypto.getRandomValues(new Uint8Array(len)).map(x => '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'.charCodeAt(x % 58))),
+
+}
+
 class chat {
 
 constructor(opt) {
@@ -14,11 +20,21 @@ constructor(opt) {
 }
 
 async create(room) {
-    this.room = typeof room === 'string' ? room : 'room-' + Math.floor(Math.random() * 1e15).toString(36)
-    this.repo = Math.floor(Math.random() * 1e15).toString(36) + Math.floor(Math.random() * 1e15).toString(36)
+    this.room = typeof room === 'string' ? room : lib.rand58(20) //'QmQzCQn4puG4qu8PVysxZmscmQ5vT1ZXpqo7f58Uh9QfyY' // 46
+    this.repo = lib.rand58(12) // ipfs-' + Math.random()
+    console.log(this)
     this.node = await this.opt.ipfs.create({
-        repo: this.repo,
-        //init: {algorithm: 'ed25519'}
+        repo: this.repo, 
+        EXPERIMENTAL: {pubsub: true},
+        /*
+        config: {
+            Addresses: {
+                Swarm: [
+                    '/dns4/star-signal.cloud.ipfs.team/tcp/443/wss/p2p-webrtc-star/ipfs/' + this.room
+                ]
+            }
+        }
+        */
     })
     //const status = this.node.isOnline() ? 'online' : 'offline'
     //const { cid } = await node.add('Hello world!')
@@ -32,3 +48,39 @@ async create(room) {
 module.exports = {
     Chat: opt => new chat(opt)
 }
+
+// (async () => { for await (const data of chat.node.cat('QmQzCQn4puG4qu8PVysxZmscmQ5vT1ZXpqo7f58Uh9QfyY') ) console.log(data.toString()) } )()
+
+/*
+{
+        repo: this.repo, // this.node.repo.stat().then(console.log) - repoPath
+        EXPERIMENTAL: {pubsub: true},
+        /*
+        init: {algorithm: 'ed25519'},
+        config: {
+            Addresses: {
+                Swarm: [
+                    '/dns4/star-signal.cloud.ipfs.team/tcp/443/wss/p2p-webrtc-star/ipfs/' + room.id,
+                // This is a public webrtc-star server
+                '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+                '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+                '/ip4/127.0.0.1/tcp/13579/wss/p2p-webrtc-star'
+              ]
+            },
+            // If you want to connect to the public bootstrap nodes, remove the next line
+            Bootstrap: []
+        },
+        libp2p: {
+            config: {
+              transport: {
+                // This is added for local demo!
+                // In a production environment the default filter should be used
+                // where only DNS + WSS addresses will be dialed by websockets in the browser.
+                [transportKey]: {
+                  filter: filters.all
+                }
+              }
+            }
+        }
+    }
+*/
